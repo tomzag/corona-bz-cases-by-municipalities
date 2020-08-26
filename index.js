3775765f9e6d20c0ab211eba008171f0034fa472
Thomas Zagler <thomas@pixelsucht.net>
2020-08-26 14:28:56 +0200

    Data from 26-8-2020

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fse = require("fs-extra");
 
 // Scrape data from this URL
 // URL has to be changed manually every day
-const url = "https://www.sabes.it/de/news.asp?aktuelles_action=300&aktuelles_image_id=1078068";
+const url = "https://www.sabes.it/de/news.asp?aktuelles_action=300&aktuelles_image_id=1078200";
 
 const listOfMunicipalities = [
     "ALDINO",
